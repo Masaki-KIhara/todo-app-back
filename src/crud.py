c52811fe8159ce6a8d5c39c55331689cd427ec0c
Masaki Kihara <masaki1224.1998@gmail.com>
2024-05-21 22:38:58 +0900

    編集時のschemaを修正

--- src/crud.py
+++ src/crud.py
@@ -15,7 +15,7 @@ def get_tasks(db:Session,skip:int=0,limit:int=100):
     return db.query(models.Task).offset(skip).limit(limit).all()
 
 def create_task(db:Session,item:schemas.CreateTask):
-    db_task = models.Task(title=item.title,is_complete=item.is_complete)
+    db_task = models.Task(title=item.title)
     db.add(db_task)
     db.commit()
     db.refresh(db_task)
@@ -29,11 +29,8 @@ def delete_task(db:Session,item_id:int):
     db.commit()
     return db.query(models.Task).offset(0).limit(100).all()
 
-
-
-
-def update_task(db:Session,item_id:int,item:schemas.CreateTask):
-    db_task:schemas.CreateTask = db.query(models.Task).filter(models.Task.id == item_id).one()
+def update_task(db:Session,item_id:int,item:schemas.UpdateTask):
+    db_task:schemas.UpdateTask = db.query(models.Task).filter(models.Task.id == item_id).one()
     db_task.title = item.title
     db_task.is_complete = item.is_complete
     db.commit()
--- src/schemas.py
+++ src/schemas.py
@@ -15,15 +15,17 @@ from datetime import datetime
 
 class CreateTask(BaseModel):
     title:str 
+
+class UpdateTask(CreateTask):
     is_complete:bool
 
 class DeleteTask(BaseModel):
     id:int
 
-
 class BaseTask(CreateTask):
     id:int
     created_at:datetime
+    is_complete:bool
     # owner_id:int
 
     class Config:
